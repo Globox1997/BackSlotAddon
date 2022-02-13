06457f00af5fc556e0bd16c87ea642dadaa648e4
Globox1997 <mitschke11@web.de>
2022-02-13 11:44:28 +0100

    updated dependencies

--- src/main/java/net/backslotaddon/BackSlotAddonClient.java
+++ src/main/java/net/backslotaddon/BackSlotAddonClient.java
@@ -1,7 +1,7 @@
 package net.backslotaddon;
 
-import grondag.frex.api.event.HeldItemLightListener;
-import grondag.frex.api.light.ItemLight;
+import io.vram.frex.api.light.HeldItemLightListener;
+import io.vram.frex.api.light.ItemLight;
 import net.backslotaddon.config.ConfigInit;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
@@ -12,7 +12,7 @@ public class BackSlotAddonClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         if (ConfigInit.CONFIG.allow_lantern_on_belt && FabricLoader.getInstance().isModLoaded("canvas")) {
-            HeldItemLightListener.EVENT.register((holdingEntity, heldStack, defaultResult, currentResult) -> {
+            HeldItemLightListener.register((holdingEntity, heldStack, defaultResult, currentResult) -> {
                 if (holdingEntity instanceof PlayerEntity && !((PlayerEntity) holdingEntity).getInventory().getStack(42).isEmpty()
                         && ((PlayerEntity) holdingEntity).getInventory().getStack(42).isIn(BackSlotAddonMain.LANTERN_TAG)) {
                     return ItemLight.get(((PlayerEntity) holdingEntity).getInventory().getStack(42));

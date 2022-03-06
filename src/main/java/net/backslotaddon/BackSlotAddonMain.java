72a9c4669c1146fcef7902be1cd8c935552ef2d6
Globox1997 <mitschke11@web.de>
2022-03-06 17:47:51 +0100

    updated dependencies

--- src/main/java/net/backslotaddon/BackSlotAddonMain.java
+++ src/main/java/net/backslotaddon/BackSlotAddonMain.java
@@ -2,14 +2,14 @@ package net.backslotaddon;
 
 import net.backslotaddon.config.ConfigInit;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.tag.TagFactory;
 import net.minecraft.item.Item;
-import net.minecraft.tag.Tag;
+import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class BackSlotAddonMain implements ModInitializer {
 
-    public static final Tag<Item> LANTERN_TAG = TagFactory.ITEM.create(new Identifier("backslotaddon", "belt_lantern_items"));
+    public static final TagKey<Item> LANTERN_TAG = TagKey.of(Registry.ITEM_KEY, new Identifier("backslotaddon", "belt_lantern_items"));
 
     @Override
     public void onInitialize() {
